c46abc39cca6e4a0c8daef9645aeda7e2c386f5e
David Moste <60351405+dmoste@users.noreply.github.com>
2023-02-28 16:51:31 +0000

    added encode

--- encryption_non_pke/encode.py
+++ encryption_non_pke/encode.py
@@ -5,27 +5,26 @@
 import random
 
 def encode(message):
-  alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ,.?!1234567890"
   new_alphabet = ""
 
   #Randomly map the original alphabet onto a new alphabet
-  for i in range(0,26):
+  for i in range(0,len(alphabet)-1):
     #Choose a random letter and add it into the new alphabet
     letter = alphabet[random.randint(0,len(alphabet)-1)]
     new_alphabet += letter
     #Remove the letter from the original alphabet
     alphabet = alphabet.replace(letter, "")
 
-  alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ ,.?!1234567890"
   encoded = ""
   for letter in message:
-    if letter in new_alphabet:
-      #Replace each letter in the message with the corresponding letter from the new alphabet
-      position = alphabet.index(letter)
-      encoded += new_alphabet[position]
+    #Replace each letter in the message with the corresponding letter from the new alphabet
+    position = alphabet.index(letter)
+    encoded += new_alphabet[position]
   
   print(new_alphabet)
   print(encoded)
   
-encode("HELLO")
+encode("HELLO, I HAVE ENCODED A MESSAGE WITH BASIC PUNCTUATION!")
     
