3a0e2a295a50ff651732679d35f081d093538f1c
David Moste <60351405+dmoste@users.noreply.github.com>
2023-03-23 22:22:38 +0000

    added sql homework

--- databases/homework.sql
+++ databases/homework.sql
@@ -14,20 +14,27 @@ INSERT INTO cats (name, age, gender) VALUES
 ("Shadow", 7, "F");
 
 --Display data
+.print "--Cats Database--"
 SELECT * FROM cats;
 
 --Display just name and age
+.print "--Display just name and age--"
 SELECT name, age FROM cats;
 
 --Display using conditionals
+.print "--Display just female cats--"
 SELECT * FROM cats WHERE gender = "F";
+
+.print "--Display just cats younger than 8 that are male--"
 SELECT * FROM cats WHERE age < 8 and gender = "M";
 
 --Display using ORDER
+.print "--Display cats orderd by age--"
 SELECT * FROM cats ORDER BY age;
 
 --Make a change using UPDATE
 UPDATE cats SET age = 10 WHERE name ="Midnight";
 
 --Display using ORDER
+.print "--Display cats ordered by name descending--"
 SELECT * FROM cats ORDER BY name DESC;
